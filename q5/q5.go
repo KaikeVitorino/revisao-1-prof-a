db9650c745b16954af10eb7249ebe93f06bd6644
KaikeVitorino <126472228+KaikeVitorino@users.noreply.github.com>
2023-04-24 11:38:37 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,6 +1,43 @@
 package q5
 
+import "fmt"
+
 func ConvertTemperature(temp float64, fromScale string, toScale string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	var Resultado float64
+	var erro error
+
+	//Funcao Switch que meio q substitui o If/Else
+	switch fromScale {
+	case "C":
+		switch toScale {
+		case "F":
+			Resultado = temp*9/5 + 32
+		case "K":
+			Resultado = temp + 273.15
+		default:
+			erro = fmt.Errorf("escala inválida")
+		}
+	case "F":
+		switch toScale {
+		case "C":
+			Resultado = (temp - 32) * 5 / 9
+		case "K":
+			Resultado = (temp-32)*5/9 + 273.15
+		default:
+			erro = fmt.Errorf("escala inválida")
+		}
+	case "K":
+		switch toScale {
+		case "C":
+			Resultado = temp - 273.15
+		case "F":
+			Resultado = (temp-273.15)*9/5 + 32
+		default:
+			erro = fmt.Errorf("escala inválida")
+		}
+	default:
+		erro = fmt.Errorf("escala inválida")
+	}
+
+	return Resultado, erro
 }
